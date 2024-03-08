69f419276739e474c5a7175d98f1f3dc87ff47fa
HM <hieumdd@gmail.com>
2024-03-08 18:20:18 +0700

    fix: Update new version

--- src/facebook/api.service.ts
+++ src/facebook/api.service.ts
@@ -1,21 +1,44 @@
 import axios from 'axios';
-
-type DopplerSecretResponse = { value: { raw: string } };
+import qs from 'query-string';
 
 export const getClient = async () => {
-    const API_VER = 'v18.0';
-
     const accessToken = await axios
-        .request<DopplerSecretResponse>({
+        .request<{ value: { raw: string } }>({
             method: 'GET',
             url: 'https://api.doppler.com/v3/configs/config/secret',
-            params: { project: 'eaglytics', config: 'prd', name: 'FACEBOOK_ACCESS_TOKEN' },
-            auth: { username: process.env.DOPPLER_TOKEN || '', password: '' },
+            params: { project: 'facebook', config: 'master', name: 'USER_ACCESS_TOKEN' },
+            auth: { username: process.env.DOPPLER_TOKEN ?? '', password: '' },
         })
         .then(({ data }) => data.value.raw);
 
-    return axios.create({
-        baseURL: `https://graph.facebook.com/${API_VER}`,
+    const apiVersion = await axios
+        .request({
+            method: 'GET',
+            url: 'https://graph.facebook.com/me',
+            params: { access_token: accessToken },
+        })
+        .then((response) => <string>response.headers['facebook-api-version']);
+
+    const client = axios.create({
+        baseURL: `https://graph.facebook.com/${apiVersion}`,
         params: { access_token: accessToken },
+        paramsSerializer: { serialize: (value) => qs.stringify(value, { arrayFormat: 'comma' }) },
     });
+
+    client.interceptors.response.use(
+        (response) => response,
+        (error) => {
+            const meta = axios.isAxiosError(error)
+                ? {
+                      config: error.config,
+                      response: { data: error.response?.data, headers: error.response?.headers },
+                  }
+                : error;
+
+            console.error({ error: meta });
+            throw error;
+        },
+    );
+
+    return client;
 };
